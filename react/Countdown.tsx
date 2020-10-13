a52b6cc30d903c521dbbbd58f9c7463a7356aaef
Jefferson Lopo <jefferson.lopo@acct.global>
2020-10-13 09:32:46 -0300

    Finalziado

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+import { useCssHandles } from "vtex.css-handles"
+import { useQuery } from 'react-apollo'
+import  useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
+
+
+const CSS_HANDLES = ['countdown']
+
 
 interface CountdownProps { }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+const Countdown: StorefrontFunctionComponent<CountdownProps> = () => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const { product } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText
+    },
+    ssr: false
+  })
+
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    )
+  }
+  
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+
+  tick(data?.product?.releaseDate, setTime)
+
+  return (
+    <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+            title: 'Final date',
+            description: 'Final date used in the countdown',
+            type: 'string',
+            default: null
+    }
+  },
 }
 
 export default Countdown
